c377e918cfc66325ce5c65a83aec2e60d5f42161
maddc0de <maddc0de@outlook.com>
2023-07-14 00:01:47 +0100

    hamburger style menu shows when screen size is smaller, replacing the row of links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom'
 import { navLinks } from '../constants'
 
 import { styles } from '../styles'
-import { logo } from '../assets'
+import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
 
   const handleLogoClick = () => {
     setActive("");
     window.scrollTo(0, 0);
   }
 
+  const handleMenuClick = () => { setToggle(!toggle); }
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex item-center gap-2" onClick={handleLogoClick}>
+        <Link to="/" className="flex item-center gap-2 " onClick={handleLogoClick}>
           <img src={logo} alt="logo" className="w-40 h-9 object-contain" />
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -31,6 +34,27 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu } alt="menu" className="w-[28px] h=[28px] object-contain cursor-pointer" onClick={handleMenuClick}
+          />
+
+          <div className={`${!toggle ? "hidden" : "flex"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li key={nav.id} 
+                className={`${active === nav.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]` }
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(nav.title);
+                }}
+                >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
