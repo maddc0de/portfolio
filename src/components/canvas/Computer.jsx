c577ddec01e48699555e6719eda4dc41c66e13a4
maddc0de <maddc0de@outlook.com>
2023-07-14 13:25:38 +0100

    adjust lighting on the computer 3D model

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -9,6 +9,8 @@ const Computer = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black'/>
       <pointLight intensity={1}/>
+      <spotLight position={[ -20, 50, 10]} angle={0.12} penumbra={1} intensity={1}/>
+
       <primitive 
         object={computer.scene}
         scale={0.75}
